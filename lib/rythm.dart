dab5ffddd0409bbb50187e15562a3da858ea5818
Freewind <pli@thoughtworks.com>
2013-07-14 23:50:11 +0800

    add pathos

--- lib/rythm.dart
+++ lib/rythm.dart
@@ -4,6 +4,7 @@ import "package:petitparser/petitparser.dart";
 import "package:petitparser/beta.dart";
 import "dart:collection";
 import "dart:io";
+import "package:pathos/path.dart" as path;
 
 part "src/helpers.dart";
 part "src/parsers.dart";
